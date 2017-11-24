d5c1de14a4a760637a3bde68a7689dd224ded67e
王金辉 <wangjh@bsfit.com.cn>
2017-11-24 16:00:12 +0800

    login.js

--- onlineRetailer-admin/src/main/webapp/js/login.js
+++ onlineRetailer-admin/src/main/webapp/js/login.js
@@ -5,14 +5,12 @@ app.controller('loginCtrl', function($scope, $http) {
 			method: 'POST',
 		    url: '/onlineRetailer-admin/onlineRetailer/login.action',
 		    data: {
-				user: {
 					name: $scope.name,
 					password: $scope.password,
 					id: "111",
 					email: "18351451741@163.com",
 					phone: "18351451741",
 					address: "kunming"
-				}
 			}
 		}).then(function successCallback(response) {
 			}, function errorCallback(response) {
